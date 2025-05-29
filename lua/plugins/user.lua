ad577564c7c3a3c3027444173d0fcce12e825010
Nertskull <nertskull@gmail.com>
2025-05-29 09:41:03 -0400

    updated configs

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+#if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
@@ -45,8 +45,10 @@ return {
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
 
-  -- Added to get neo-tree to show files 
-  { opts = {
+  -- Added to get neo-tree to show hidden dot-files 
+  { 
+    "neo-tree/neo-tree.nvim",
+    opts = {
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
